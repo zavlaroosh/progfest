98b9bed958e1901b7815100ecf4ebc804861a775
Xavier Larouche <xavierlarouche99@gmail.com>
2022-03-16 15:48:56 -0400

    dev 1 #1b

--- main.py
+++ main.py
@@ -1,43 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-path = r'C:\Users\Utilisateur\Desktop\ulaval\Été 2021\python\Travaux pratiques\tp2_Xavier_Larouche\pythonProject1\AtelierVisualisation\progFest_OOPUnitTests\data/2022-02-30_experiment1.txt'
-class EncabulatorData():
-    def __init__(self):
-        self.fich = open(path,'r')
-        self.name = path[-25:]
-        self.datay = []
-        self.datax = []
-        self.metadata = []
-    def readfile(self):
-        data = open(path,'r')
-        if '.csv' in path:
-            self.metadata.append(data.readline())
-            self.metadata.append(data.readline())
-            for lines in data.readlines():
-                data = lines.split(self,',')
-            for i in data:
-                if i % 2 == 0:
-                    self.datax.append(i)
-                else:
-                    self.datay.append(i)
+delta = []
+x = 0
+while x <= 10:
+    delta.append(x*(np.pi))
+    x += 0.1
+phis = []
 
-        elif '.txt' in path:
-            data = open(path, 'r')
-            if path[-3:] == 'txt':
-                self.metadata.append(data.readline())
-                self.metadata.append(data.readline())
-                for lines in data.readlines():
-                    data = lines.split(self)
-                for i in data:
-                    if i % 2 == 0:
-                        self.datax.append(i)
-                    else:
-                        self.datay.append(i)
+for i in delta:
+    phi = np.arctan(((1 - (0.95 ** 2) * np.sin(i)) / ((2 * 0.95) + ((1 + (0.95 ** 2)) * np.cos((i))))))
+    phis.append(phi)
 
-
-
-y = EncabulatorData().readfile().datay
-x = EncabulatorData().readfile().datax
+y = phis
+x = delta
 
 plt.plot(x,y)
 plt.show()
